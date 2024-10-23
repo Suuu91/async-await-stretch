47cf442c34deae1c935009dd1121fdbde518c45a
zhejun su <suzhejunk9@gmail.com>
2024-10-23 10:19:32 -0400

    initial commit and add functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+const getArtists = async () => {
+  const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api/2409-FTB-ET-WEB-FT/artists`)
+  const artistsInfo = await response.json()
+  const artistsName = artistsInfo.data.map((item) => {
+    return `<li>${item.name}</li>`
+  })
+
+  const ol = document.querySelector(`ol`)
+  ol.innerHTML = artistsName.join()
+}
+
+getArtists()
